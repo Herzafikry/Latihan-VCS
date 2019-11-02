5bf3ff631e15f219c8d9643cd394335aaa5f8bb7
Muhamad Herza Fikry <56195644+Herzafikry@users.noreply.github.com>
2019-11-02 13:42:09 +0700

    Add files via upload

---
+++ Tugas1.py
@@ -0,0 +1,11 @@
+#Ini Variabel & Nia
+
+namalengkap = "Muhamad Herza Fikry"
+namapanggilan = "Herza"
+npm = "311910618"
+tempatlahir = "Tangerang"
+umur = "19"
+telf = "08980027530"
+alamat = "Lemah Abang"
+
+print("Assalamualaikum, \n Let me introduce my self. My name is",namalengkap,", but you call me",namapanggilan,",my npm is",npm,",I was born in",tempatlahir,"and i am",umur,"years old.I am very glad if you want to invite my house in",alamat,",So, don't forget me to call me before with the number",telf,"\n Thank you")
